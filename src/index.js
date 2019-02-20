22d7637fedb87ea61900523da66a7e4af4ed3160
Alex Miranda <alexxmirandaa@gmail.com>
2019-02-20 01:51:41 -0300

    pegando a porta do process

--- src/index.js
+++ src/index.js
@@ -4,75 +4,71 @@ import bodyParser from 'body-parser';
 import { execquery, connection } from './queries';
 import { PasswordCreate, PasswordCompare } from './utils/password';
 
-const port = 8080;
-var server = app.listen(process.env.PORT || port, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
-app.get('/', (req, res) => res.send('ola'))
-// const corsOptions = {
-//   origin: 'http://localhost:9000',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+const port = process.env.PORT || 9002;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use('/', router);
+const corsOptions = {
+  origin: 'http://localhost:9000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-// app.listen(port, () => {
-//   console.log(`server rodando na portela ${port}`);
-// })
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', router);
 
-// router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.listen(port, () => {
+  console.log(`server rodando na portela ${port}`);
+})
 
-// router.get('/clientes', cors(corsOptions), (req, res) => {
-//   execquery('SELECT * FROM tb_users', [], (queryResponse) => {
-//     res.json(queryResponse.result);
-//     console.log('>>>', queryResponse.result);
-//   } );
-// })
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
-// router.get('/cliente/:id', (req, res) => {
-//   res.setHeader('Content-Type', 'application/json')
-//   console.log("req.body", req);
-//   execquery('SELECT * FROM tb_users WHERE IDuser=?', res, [req.params.id]);
-// })
+router.get('/clientes', cors(corsOptions), (req, res) => {
+  execquery('SELECT * FROM tb_users', [], (queryResponse) => {
+    res.json(queryResponse.result);
+    console.log('>>>', queryResponse.result);
+  } );
+})
 
-// router.post('/cadastro', (req, res) => {
-//   const query = `insert into tb_users (nome, sobrenome, email, senha, ddd, avaliacaoProfissional, IDcategoriaUser, IDservicoUser, oauth_provider, oauth_uid, picture, link, created, modified, locale, gender, codCliente, codClienteEncript) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-//   const arrayValues = [req.body.nome, req.body.sobreNome, req.body.email, PasswordCreate(req.body.senha, 10), '', '', 0, 0, '', '', '', '', '', '', '', '', 0, 0]
+router.get('/cliente/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  console.log("req.body", req);
+  execquery('SELECT * FROM tb_users WHERE IDuser=?', res, [req.params.id]);
+})
+
+router.post('/cadastro', (req, res) => {
+  const query = `insert into tb_users (nome, sobrenome, email, senha, ddd, avaliacaoProfissional, IDcategoriaUser, IDservicoUser, oauth_provider, oauth_uid, picture, link, created, modified, locale, gender, codCliente, codClienteEncript) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const arrayValues = [req.body.nome, req.body.sobreNome, req.body.email, PasswordCreate(req.body.senha, 10), '', '', 0, 0, '', '', '', '', '', '', '', '', 0, 0]
   
-//   execquery(query, arrayValues, (queryResponse) =>  {
-//     if(queryResponse.result.codeResult === 0) {
-//       const success = Object.assign(queryResponse.result, {data: null});
-//       res.json(success);
-//     } else {
-//       const error = Object.assign(queryResponse.result, {codeRestult: 1, errorMsg: 'Cadastro não realizado', data: null});
-//       res.json(error);
-//     }
-//     if(!queryResponse.result)
-//       queryResponse.con.end();
-//   });
-// })
+  execquery(query, arrayValues, (queryResponse) =>  {
+    if(queryResponse.result.codeResult === 0) {
+      const success = Object.assign(queryResponse.result, {data: null});
+      res.json(success);
+    } else {
+      const error = Object.assign(queryResponse.result, {codeRestult: 1, errorMsg: 'Cadastro não realizado', data: null});
+      res.json(error);
+    }
+    if(!queryResponse.result)
+      queryResponse.con.end();
+  });
+})
 
-// router.post('/login', (req, res) => {
-//   const query = 'SELECT senha FROM tb_users WHERE email = ?';
-//   const arrayValues = [req.body.email]
+router.post('/login', (req, res) => {
+  const query = 'SELECT senha FROM tb_users WHERE email = ?';
+  const arrayValues = [req.body.email]
 
-//   execquery(query, arrayValues, (queryResponse) =>  {
-//     let confirmPassword = PasswordCompare(req.body.senha, queryResponse.result.data[0].senha);
+  execquery(query, arrayValues, (queryResponse) =>  {
+    let confirmPassword = PasswordCompare(req.body.senha, queryResponse.result.data[0].senha);
 
-//     if(confirmPassword) {
-//       const successPassword = Object.assign(queryResponse.result, {data: null})
-//       res.json(successPassword)
-//     } else {
-//       const errorPassword = Object.assign(queryResponse.result, {codeRestult: 1, errorMsg: 'Senha inválida', data: null});
-//       res.json(errorPassword)
-//     }
+    if(confirmPassword) {
+      const successPassword = Object.assign(queryResponse.result, {data: null})
+      res.json(successPassword)
+    } else {
+      const errorPassword = Object.assign(queryResponse.result, {codeRestult: 1, errorMsg: 'Senha inválida', data: null});
+      res.json(errorPassword)
+    }
 
-//     if(!queryResponse.result)
-//       queryResponse.con.end();
-//   });
+    if(!queryResponse.result)
+      queryResponse.con.end();
+  });
 
-// })
+})
